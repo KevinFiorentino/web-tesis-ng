12cc419c75c1e4b7043bf9aec5d0440b9825ce66
KevinFiorentino <kevinfiorentino@hotmail.com>
2020-03-23 15:39:11 -0300

    servicio https, pronostico ciudad

--- src/app/pronostico-hoy/pronostico-hoy.component.ts
+++ src/app/pronostico-hoy/pronostico-hoy.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
 	selector: 'app-pronostico-hoy',
 	templateUrl: './pronostico-hoy.component.html',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PronosticoHoyComponent implements OnInit {
+
 
 	inputBuscarCiudad: string;
 
@@ -27,24 +30,29 @@ export class PronosticoHoyComponent implements OnInit {
 
 	error: string;
 
-	constructor() {
+	constructor(public httpClient: HttpClient) {
 
-		this.descrip = '.....';
-		this.icon = 'public/images/icons/clima_default.png';
-		this.temp = '--';
-		this.temp_min = '--';
-		this.temp_max = '--';
-		this.press = '-';
-		this.hum = '-';
-		this.vis = '-';
-		this.wind = '-';
-		this.sunrise = '--:--';
-		this.sunset = '--:--';
-		this.id = '0';
-		this.city = '---';
-		this.fecha = '---';
+		var meses 	= new Array ("Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+			"Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+		var f 		= new Date()
+		var fecha 	= f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear()
 
-		this.error = '';
+		this.descrip 	= '.....';
+		this.icon 		= 'public/images/icons/clima_default.png';
+		this.temp 		= '--';
+		this.temp_min 	= '--';
+		this.temp_max 	= '--';
+		this.press 		= '-';
+		this.hum 		= '-';
+		this.vis 		= '-';
+		this.wind 		= '-';
+		this.sunrise 	= '--:--';
+		this.sunset 	= '--:--';
+		this.id 		= '';
+		this.city 		= '---';
+		this.fecha 		= fecha;
+
+		this.error 		= '';
 
 	}
 
@@ -52,8 +60,65 @@ export class PronosticoHoyComponent implements OnInit {
 
 	buscarCiudad(): void {
 
-		this.city = this.inputBuscarCiudad;
+		this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Bogota&units=metric&appid=f3f376b99fe63334a561bad62acb4f94')
+		.subscribe((response) => {
 
+			var sunrise = new Date(response.sys.sunrise * 1000)
+			var sunrise_hours = sunrise.getHours()
+			var sunrise_minutes = "0" + sunrise.getMinutes()
+
+			var sunset = new Date(response.sys.sunset * 1000)
+			var sunset_hours = sunset.getHours()
+			var sunset_minutes = "0" + sunset.getMinutes()
+
+			this.descrip 	= this.getDescription(response.weather[0].icon);
+			this.icon 		= 'public/images/icons/' + response.weather[0].icon + '.svg';
+			this.temp 		= (Math.round(response.main.temp)).toString();
+			this.temp_min 	= (Math.floor(response.main.temp_min)).toString();
+			this.temp_max 	= (Math.ceil(response.main.temp_max)).toString();
+			this.press 		= response.main.pressure;
+			this.hum 		= response.main.humidity;
+			this.vis 		= response.visibility;
+			this.wind 		= response.wind.speed;
+			this.sunrise 	= sunrise_hours + ':' + sunrise_minutes.substr(-2);
+			this.sunset 	= sunset_hours + ':' + sunset_minutes.substr(-2);
+			this.id 		= response.id;
+			this.city 		= response.name;
+
+        });
+
+	}
+
+	getDescription(icon: string) {
+		switch(icon) {
+			case '01d': case '01n': {
+				return "Cielo despejado";
+			};break;
+			case '02d': case '02n': {
+				return "Algunas nubes";
+			};break;
+			case '03d': case '03n': {
+				return "Nublado";
+			};break;
+			case '04d': case '04n': {
+				return "Nublado";
+			};break;
+			case '09d': case '09n': {
+				return "Probabilidad de lluvias";
+			};break;
+			case '10d': case '10n': {
+				return "Lluvioso";
+			};break;
+			case '11d': case '11n': {
+				return "Tormenta eléctrica";
+			};break;
+			case '13d': case '13n': {
+				return "Nevadas";
+			};break;
+			case '50d': case '50n': {
+				return "Niebla";
+			};break;
+		}
 	}
 
 }
