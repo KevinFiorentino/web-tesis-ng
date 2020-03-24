9c1f0cf35471272c8bf551011a44d590436d6665
KevinFiorentino <kevinfiorentino@hotmail.com>
2020-03-24 17:27:12 -0300

    lista de ciudades terminada

--- src/app/pronostico-ciudades/pronostico-ciudades.component.ts
+++ src/app/pronostico-ciudades/pronostico-ciudades.component.ts
@@ -10,7 +10,6 @@ import { Ciudad } from './ciudad';
 	styleUrls: ['./pronostico-ciudades.component.css']
 })
 
-
 export class PronosticoCiudadesComponent implements OnInit {
 
 	add_ciudad: string;
@@ -20,12 +19,10 @@ export class PronosticoCiudadesComponent implements OnInit {
 	//Pasamos el servicio HTTP como parámetro al constructor
 	constructor(public httpClient: HttpClient) {
 
-		let cities = ["Bogota", "Buenos Aires", "Hong Kong", 
-			"Nueva York", "Madrid", "Mosku", "Paris", 
-			"Roma", "Sidney", "Viena"];
+		let cities = ["Bogota", "Buenos Aires", "Hong Kong", "Nueva York", "Madrid", "Mosku", 
+			"Paris", "Roma", "Sidney", "Viena"];
 
-		cities.forEach(function (value) {
-			console.log(value);
+		cities.forEach(value => {
 
 			this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=' + value + '&units=metric&appid=f3f376b99fe63334a561bad62acb4f94')
 			.subscribe((response) => {
@@ -43,35 +40,14 @@ export class PronosticoCiudadesComponent implements OnInit {
 				this.ciudadesList.push(city);
 
 			});
-
 		});
-
-/*
-		this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=' + cities[1] + '&units=metric&appid=f3f376b99fe63334a561bad62acb4f94')
-		.subscribe((response) => {
-
-			let city = new Ciudad(
-				'public/images/icons/' + response['weather'][0]['icon'] + '.svg', 
-				(Math.round(response['main']['temp'])).toString(), 
-				(Math.floor(response['main']['temp_min'])).toString(), 
-				(Math.ceil(response['main']['temp_max'])).toString(), 
-				response['main']['humidity'], 
-				response['name'], 
-				response['id']
-			);
-
-			this.ciudadesList.push(city);
-
-		});
-*/
 
 	}
 
 	ngOnInit(): void {}
 
-
 	addCiudadTable(): void {}
 
-	removeCiudadTable():void {}
+	removeCiudadTable(): void {}
 
 }
